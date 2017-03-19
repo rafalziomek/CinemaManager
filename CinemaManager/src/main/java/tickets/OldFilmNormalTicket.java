10dc7e2fa92468c51d339231865937fb7b6bd5a0
Rafal Ziomek <rafalziomek@onet.pl>
2017-03-19 16:11:05 +0100

    Some test cleanup

---
+++ CinemaManager/src/main/java/tickets/OldFilmNormalTicket.java
@@ -0,0 +1,10 @@
+package tickets;
+
+import enums.TicketType;
+
+public class OldFilmNormalTicket extends Ticket {
+	public OldFilmNormalTicket() {
+		price = 14;
+		typeDescription = "Old Film Normal";
+	}
+}
